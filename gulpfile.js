a3efbfe8a772c117aff57c46f4a7a65bfbc1bece
Ezeikel Pemberton <ezeikel.pemberton@cohaesus.co.uk>
2015-11-13 14:27:02 +0000

    Swapped gulp-sass for the ruby version. Watch moved to styles.js file.

--- gulpfile.js
+++ gulpfile.js
@@ -4,31 +4,17 @@ var tasks = requireDir('./gulp');
 var guppy = require('git-guppy')(gulp);
 
 function startScripts() {
-  tasks.styles.compile(true);
+  //tasks.styles.compile(true);
 }
 
 gulp.task('build', function() { return tasks.scripts.compile(); });
 gulp.task('start', function() { return startScripts(); });
 
 // Default development tasks
-gulp.task('default', ['scripts', 'html', 'connect']);
+gulp.task('default', ['scripts', 'html', 'sass', 'connect']);
 
 // Testing task
 gulp.task('test', ['html-lint', 'js-lint']);
 
 // Test code on commit
 gulp.task('pre-commit', ['test']);
-
-// Watch files
-// gulp.task('watch', function() {
-
-//   // Watch .js files
-//   gulp.watch('src/js/*js', ['build']);
-
-//   // Watch .scss files
-//   gulp.watch(['src/sass/**/*scss', 'src/sass/*scss'], function() {
-//     return tasks.styles.compile();
-//   });
-// });
-
-// gulp.task('default', ['start', 'lint', 'connect', 'watch']);
